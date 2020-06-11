9a1f0eca4981a1f24d5d12b3443df324a7823d19
Rhys <rhys-stephens@hotmail.co.uk>
2020-06-11 17:48:33 +0100

    Created database models

--- pawtyplannerbackend/models.py
+++ pawtyplannerbackend/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    price = models.FloatField()
+    picture = models.ImageField()
+
+
+class Design(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    picture = models.ImageField()
+
+
+class Order(models.Model):
+    order_number = models.IntegerField(unique=True)
+    price = models.FloatField()
+
+
+class ProductOrder(models.Model):
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    design = models.ForeignKey('Design', on_delete=models.CASCADE, blank=True)
+    quantity = models.IntegerField()
+    order = models.ForeignKey('Order', on_delete=models.CASCADE)
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=50)
+
